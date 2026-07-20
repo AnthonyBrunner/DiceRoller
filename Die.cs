e8669c26fb82c444a8a716b0aa507d9b80b39e35
AnthonyBrunner <abrunner5361@students.cptc.edu>
2026-07-20 15:42:37 -0700

    Add Die class with validation and documentation
    Introduced a Die class in the DiceRoller namespace to represent a die with 1–20 sides. Includes input validation, a read-only NumberOfSides property, and XML documentation. Added necessary using directives.

---
+++ Die.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiceRoller;
+
+/// <summary>
+/// Highlighting then pressing shift+tab will remove the indentation
+/// </summary>
+
+/// <summary>
+/// This represents a configurable die used in games or simulations
+/// </summary>
+public class Die
+{
+    public Die(byte numSides)
+    {
+        if (numSides == 0 || numSides > 20)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numSides), 
+                $"{nameof(numSides)} must be greater than 0 and less than 21");
+        }
+        NumberOfSides = numSides;
+    }
+
+    /// <summary>
+    /// Gets the number of sides for the die
+    /// </summary>
+    public byte NumberOfSides { get; private set; }
+}
